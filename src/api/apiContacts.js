35a515e2627fd110d7245bfdd7990cceb78c2a25
ToreadorUa <99kava@gmail.com>
2023-09-14 17:44:37 +0200

    upd

--- src/api/apiContacts.js
+++ src/api/apiContacts.js
@@ -3,30 +3,17 @@ import axios from 'axios';
 const BASE_URL = 'https://64fcb2a3605a026163aebe9d.mockapi.io/api/v1';
 
 export const getContacts = async () => {
-  try {
-    const { data } = await axios.get(`${BASE_URL}/contacts`);
-    console.log(data);
-    return data;
-  } catch (error) {
-    return error.message;
-  }
+  const resp = await axios.get(`${BASE_URL}/contacts`);
+  console.log(resp);
+  return resp;
 };
 
 export const addContact = async contact => {
-  try {
-    const resp = await axios.post(`${BASE_URL}/contacts`, contact);
-    return resp.data;
-  } catch (error) {
-    return error.message;
-  }
+  const resp = await axios.post(`${BASE_URL}/contacts`, contact);
+  return resp;
 };
 
 export const delContact = async id => {
-  try {
-    const resp = await axios.delete(`${BASE_URL}/contacts/${id}`);
-    console.log(resp.data.id);
-    return resp.data.id;
-  } catch (error) {
-    return error.message;
-  }
+  const resp = await axios.delete(`${BASE_URL}/contacts/${id}`);
+  return resp;
 };
--- src/redux/thunk.js
+++ src/redux/thunk.js
@@ -1,14 +1,41 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { addContact, delContact, getContacts } from 'api/apiContacts';
 
-export const getContactsThunk = createAsyncThunk('contacts/get', () =>
-  getContacts()
+export const getContactsThunk = createAsyncThunk(
+  'contacts/get',
+  async (_, { rejectWithValue }) => {
+    try {
+      const resp = await getContacts();
+      if (resp.status !== 200) throw new Error('Something went wrong');
+      return resp.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
-export const addContactThunk = createAsyncThunk('contacts/add', contact =>
-  addContact(contact)
+export const addContactThunk = createAsyncThunk(
+  'contacts/add',
+  async (contact, { rejectWithValue }) => {
+    try {
+      const resp = await addContact(contact);
+      if (resp.status !== 201) throw new Error('Something went wrong');
+      return resp.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
 
-export const delContactThunk = createAsyncThunk('contacts/delete', id =>
-  delContact(id)
+export const delContactThunk = createAsyncThunk(
+  'contacts/delete',
+  async (id, { rejectWithValue }) => {
+    try {
+      const resp = await delContact(id);
+      if (resp.status !== 200) throw new Error('Something went wrong');
+      return resp.data.id;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
 );
